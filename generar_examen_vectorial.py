19e8f16e50fdac3245457a179ad411b7cdcd0ced
Lorenzo Pérez B <lorenzo.perez@funiber.org>
2026-05-13 11:48:36 +0200

    workflow fixed 3

--- generar_examen_vectorial.py
+++ generar_examen_vectorial.py
@@ -193,12 +193,12 @@ def generar_doc_unico(asignaciones):
         # =====================================
 
         doc.add_page_break()
-        doc.add_page_break()
+        # doc.add_page_break()
 
     doc.save("examenes_vectoriales.docx")
 
     print("✅ DOC FINAL GENERADO: examenes_vectoriales.docx")
-    
+
 # =========================================================
 # MAIN
 # =========================================================
